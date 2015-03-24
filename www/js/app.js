fe42717521471c90cde09e1cdb2a678822652ca2
Paul_Fagan <paulfagan1991@gmail.com>
2015-03-24 17:18:32 -0400

    Created "Add Task" Modal

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,33 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  $scope.tasks = [];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  $scope.createTask = function(task) {
+  $scope.tasks.push({
+    title: task.title
+  });
+  $scope.taskModal.hide();
+  task.title = "";
+};
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
+
+});
